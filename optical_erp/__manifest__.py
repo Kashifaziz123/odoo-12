eeebb3a664184febe30c9d24f889e53450306db8
kashifaziz123 <kashifaziz123@hotmail.com>
2019-08-04 17:57:14 +0500

    optical_erp

---
+++ optical_erp/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Optical ERP",
+    "version": "12.0.0.0.1",
+    "category": "Optical",
+    "version": "12.0.0.0.1",
+    'summary': "Solution for Optical(EYE) shops and clinics",
+    'description': """
+    odoo Solution for Optical(EYE) shops and clinics.
+    """,
+    "author": "Kashif Aziz",
+    "website": "https://www.objectsynergy.com",
+    "license": "OPL-1",
+    'images': ['static/description/background.jpeg'],
+    "depends": [
+        'base','sale',
+    ],
+'price': 50, 'currency': 'EUR',
+    "data": [
+        "security/ir.model.access.csv",
+        "view/view.xml",
+    ],
+    "installable": True,
+}
