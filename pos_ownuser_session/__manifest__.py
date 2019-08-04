777b4080196b200582176912fba027f44fdefe96
kashifaziz123 <kashifaziz123@hotmail.com>
2019-08-04 14:33:25 +0500

    updated

--- pos_ownuser_session/__manifest__.py
+++ pos_ownuser_session/__manifest__.py
@@ -9,7 +9,7 @@
     "author": "Kashif Aziz",
     "website": "https://www.objectsynergy.com",
     "license": "OPL-1",
-   'images': ['static/description/visible_session.jpg'],
+   'images': ['static/description/visible_session (1).png'],
     "depends": [
         'base','point_of_sale',
     ],
