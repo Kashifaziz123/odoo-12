bde06231af6a667e3e3b842a9746db9ceb62492b
kashif <kashifaziz123@hotmail.com>
2020-12-25 21:24:04 +0500

    Signed-off-by: kashif <kashifaziz123@hotmail.com>

--- dr/__manifest__.py
+++ dr/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "dr",
     "version": "13.0.0.0.1",
     "category": "dr",
-    "author": "Kashif Aziz",
+    "author": "Alhaditech",
     "website": "https://www.objectsynergy.com/",
     "license": "OPL-1",
  'images': ['static/description/dr.jpg'],
