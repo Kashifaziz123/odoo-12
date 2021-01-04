b109094513fbe153d5af0eb5d0baf85d689dd771
Maknojia110 <haider.mehdi685@gmail.com>
2021-01-04 18:49:38 +0500

    Signed-off-by: Maknojia110 <haider.mehdi685@gmail.com>

---
+++ doctor/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "doctor",
+    "version": "12.0.0.0.1",
+    "category": "dr",
+    "author": "Kashif Aziz",
+    "website": "https://www.objectsynergy.com/",
+    "license": "OPL-1",
+    'images': ['static/description/dr.jpg'],
+    "depends": [
+        'base','sale',
+    ],
+    "data": [
+        "security/groups.xml",
+        "security/ir.model.access.csv",
+        "security/security.xml",
+        "view/doctor_wizard.xml",
+        "view/doctor.xml",
+    ],
+    "installable": True,
+}
