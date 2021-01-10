4e7e52feb9e470356a795384444ab5ccd872039f
kashif <kashifaziz123@hotmail.com>
2021-01-11 00:24:36 +0500

    Signed-off-by: kashif <kashifaziz123@hotmail.com>

--- create_partner_user/__manifest__.py
+++ create_partner_user/__manifest__.py
@@ -7,7 +7,7 @@
     'description': """
     odoo Solution dynamic user from partner.
     """,
-    "author": "Al-hadiTech",
+    "author": "Alhaditech",
     "website": "www.alhaditech.com",
     "license": "OPL-1",
     'images': ['static/description/background.png'],
