06f74ddabb48079ba90d58b7a2a991f6abca23b9
kashif <kashifaziz123@hotmail.com>
2021-01-11 00:23:34 +0500

    Signed-off-by: kashif <kashifaziz123@hotmail.com>

--- create_partner_user/__manifest__.py
+++ create_partner_user/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "13.0.0.0.1",
     "category": "",
     "version": "13.0.0.0.1",
-    'summary': "Create UserFrom Partner",
+    'summary': "Create User From Partner",
     'description': """
     odoo Solution dynamic user from partner.
     """,
