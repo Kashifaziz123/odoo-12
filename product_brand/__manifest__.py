6a8595ff5d1c7ed50023b286ed6832f50a9337ab
kashif <kashifaziz123@hotmail.com>
2021-02-18 00:58:46 +0500

    Signed-off-by: kashif <kashifaziz123@hotmail.com>

--- product_brand/__manifest__.py
+++ product_brand/__manifest__.py
@@ -9,12 +9,9 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Alhaditech",
     'website': "http://www.alhaditech.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+ 
     'category': 'Uncategorized',
     'version': '0.1',
 
@@ -25,7 +22,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/product_view.xml',
-        'views/views.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode
