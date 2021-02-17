33a9144960b60b0aded736fefe335b203511ad44
kashif <kashifaziz123@hotmail.com>
2021-02-17 15:24:12 +0500

    Signed-off-by: kashif <kashifaziz123@hotmail.com>

---
+++ pos_prescription_creation/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'optoal POS',
+    'version': '13.0.1.0.0',
+    'category': 'hidden',
+    'summary': 'Create Products From POS Interface',
+    'author': 'AHT',
+    'company': 'Cybrosys Techno Solutions',
+    'images': ['static/description/banner.png'],
+    'website': 'https://www.cybrosys.com',
+    'depends': ['point_of_sale'],
+    'data': [
+             'views/pos_template.xml',
+            ],
+    'qweb': ['static/src/xml/pos.xml','static/src/xml/prescription_history.xml'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+}
