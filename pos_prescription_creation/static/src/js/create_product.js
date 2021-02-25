636e843908e5310b07b3c0c8f3d09ce89e1dba8c
Zain Irfan <zainirfanwaheedi6@gmail.com>
2021-02-25 17:16:53 +0500

    Prescription Creation Form corrected,updated design in POS, correct field values in XML & Python, updated prescription history subwindow-container.

--- pos_prescription_creation/static/src/js/create_product.js
+++ pos_prescription_creation/static/src/js/create_product.js
@@ -24,7 +24,6 @@ models.load_models({
 
 models.load_models({
         model:  'dr.prescription',
-//        fields: ['id','name','dr','customer','checkup_date','test_type','prescription_type','state','od_sph_distance'],
         loaded: function(self,optical_orders){
             self.db.optical_all_orders = optical_orders;
             self.products = optical_orders;
@@ -69,41 +68,10 @@ screens.define_action_button({
  var button_book_order = screens.ActionButtonWidget.extend({
         template: 'button_book_order',
         button_click: function () {
-        var self = this;
-        this._super();
-         var optometrist = [];
-        var partner = [];
-        var test_types=[];
-        var od_sph_distances=[];
-
-        for (var i in self.pos.dr){
-            optometrist.push(self.pos.dr[i].name);
-
-        }
-         for (var i in self.pos.customers){
-            partner.push(self.pos.customers[i].name);
-        }
-         for (var i in self.pos.test_type){
-            test_types.push(self.pos.test_type[i].name);
-        }
-         for (var i in self.pos.products){
-            od_sph_distances.push(self.pos.products[i].od_sph_distance);
-
-        }
-        var abc= [];
-        for (var i=0;i<90;i++)
-            abc.push(i);
-         self.gui.show_popup('product_create',{
-                'doctors':optometrist,
-                'partners':partner,
-                'test_type':test_types,
-                'od_sph_distance':od_sph_distances,
-                'abc':abc,
-            });
-
+            var self = this;
+            this._super();
+            self.gui.show_popup('product_create');
         },
-
-
     });
 
     screens.define_action_button({
@@ -130,156 +98,62 @@ var ProductCreationWidget = PopupWidget.extend({
     show: function(options){
         options = options || {};
         this._super(options);
-        this.doctors = options.doctors;
-        this.partners = options.partners;
-        this.test_type = options.test_type;
-        this.od_sph_distances = options.od_sph_distances;
-        this.abc= options.abc;
+        this.doctors = this.pos.dr;
+        this.partners = this.pos.customers;
+        this.test_type = this.pos.test_type;
+        var abc= [];
+        for (var i=0;i<90;i++)
+            abc.push(i);
+        this.abc= abc;
         this.renderElement();
 
     },
     click_confirm: function(){
         var self = this;
         var order = this.pos.get_order();
-        var doctor = this.$('.doctor').val();
-        var partner = this.$('.partner').val();
-        var test_type = this.$('.test_type').val();
-        var age = this.$('.age').val();
-        var checkup_date = this.$('.checkup_date').val();
-        var prescription_type = this.$('.prescription_type').val();
-        var eyes_charges = this.$('.eye_charges').val();
-        var eyes_history = this.$('.eyes_history').val();
-        var ocular_history  = this.$('.ocular_history').val();
-        var consultation_reason = this.$('.consultation_reason').val();
-        var diagnosis_client = this.$('.diagnosis_client').val();
-        var notes_laboratory = this.$('.notes_laboratory').val();
-        var optometrist_observation = this.$('.optometrist_observation').val();
-        var od_sph_distance = this.$('.od_sph_distance').val();
-        var od_cyl_distance = this.$('.od_cyl_distance').val();
-        var od_axis_distance = this.$('.od_axis_distance').val();
-        var od_add_distance = this.$('.od_add_distance').val();
-        var od_prism_distance = this.$('.od_prism_distance').val();
-        var od_base_distance = this.$('.od_base_distance').val();
-        var os_sph_distance = this.$('.os_sph_distance').val();
-        var os_cyl_distance = this.$('.os_cyl_distance').val();
-        var os_ax_distance = this.$('.os_ax_distance').val();
-        var os_add_distance  = this.$('.os_add_distance').val();
-        var os_prism_distance = this.$('.os_prism_distance').val();
-        var os_base_distance = this.$('.os_base_distance').val();
-        var od_sph_near = this.$('.od_sph_near').val();
-        var od_cyl_near = this.$('.od_cyl_near').val();
-        var od_ax_near = this.$('.od_ax_near').val();
-        var od_add_near = this.$('.od_add_near').val();
-        var od_prism_near  = this.$('.od_prism_near').val();
-        var od_base_near  = this.$('.od_base_near').val();
-        var  os_sph_near = this.$('.os_sph_near').val();
-        var os_cyl_near = this.$('.os_cyl_near').val();
-        var os_ax_near = this.$('.os_ax_near').val();
-        var os_add_near = this.$('.os_add_near').val();
-        var os_prism_near = this.$('.os_prism_near').val();
-        var os_base_near = this.$('.os_base_near').val();
-
-
-
+        var vals = $("#prescription_form").serializeObject();
+        vals["dr"] = $('option:selected', $('[name=dr]')).data('id');
+        vals["customer"] = $('option:selected', $('[name=customer]')).data('id');
+        vals["test_type"] = $('option:selected', $('[name=test_type]')).data('id');
+        if (vals["dual_pd"] !== "on")
+            vals["dual_pd"] = "off";
+        vals = JSON.stringify(vals);
+        var checkup_date = $('[name=checkup_date]').val();
         var today = new Date().toJSON().slice(0,10);
         if( !checkup_date) {
               this.gui.show_popup('error',{
-                    'title': _t('Checkup date is empty'),
-                    'body':  _t('You need to select a Checkup date'),
+                'title': _t('Checkup date is empty'),
+                'body':  _t('You need to select a Checkup date'),
                 cancel: function () {
-
-                 var optometrist = [];
-        var partner = [];
-        var test_types=[];
-        var od_sph_distances=[];
-
-        for (var i in self.pos.dr){
-            optometrist.push(self.pos.dr[i].name);
-
-        }
-         for (var i in self.pos.customers){
-            partner.push(self.pos.customers[i].name);
-        }
-         for (var i in self.pos.test_type){
-            test_types.push(self.pos.test_type[i].name);
-        }
-
-
-                    this.gui.show_popup('product_create',{
-                'doctors':optometrist,
-                'partners':partner,
-                'test_type':test_types,
-
-            });
+                    this.gui.show_popup('product_create');
                 },
-
-                });
+              });
         }
         else {
-          this.gui.show_popup('confirm',{
+            this.gui.show_popup('confirm',{
                 'title': _t('Create a Prescription ?'),
                 'body': _t('Are You Sure You Want a Create a Prescription'),
-                confirm: function(){
-                  var product_vals = {
-                        'doctor':doctor,
-                        'partner':partner,
-                        'checkup_date':checkup_date,
-                        'test_type':test_type,
-                        'prescription_type':prescription_type,
-                        'eyes_charges':eyes_charges,
-                        'eyes_history':eyes_history,
-                        'ocular_history':ocular_history,
-                        'consultation_reason':consultation_reason,
-                        'diagnosis_client':diagnosis_client,
-                        'notes_laboratory':notes_laboratory,
-                        'optometrist_observation':optometrist_observation,
-                        'od_sph_distance':od_sph_distance,
-                        'od_cyl_distance':od_cyl_distance,
-                        'od_axis_distance':od_axis_distance,
-                        'od_add_distance':od_add_distance,
-                        'od_prism_distance':od_prism_distance,
-                        'od_base_distance':od_base_distance,
-                        'os_sph_distance':os_sph_distance,
-                        'os_cyl_distance':os_cyl_distance,
-                        'os_ax_distance':os_ax_distance,
-                        'os_add_distance':os_add_distance,
-                        'os_prism_distance':os_prism_distance,
-                        'os_base_distance':os_base_distance,
-                        'od_sph_near':od_sph_near,
-                        'od_cyl_near':od_cyl_near,
-                        'od_ax_near':od_ax_near,
-                        'od_add_near':od_add_near,
-                        'od_prism_near':od_prism_near,
-                        'od_base_near':od_base_near,
-                        'os_sph_near':os_sph_near,
-                        'os_cyl_near':os_cyl_near,
-                        'os_ax_near':os_ax_near,
-                        'os_add_near':os_add_near,
-                        'os_prism_near':os_prism_near,
-                        'os_base_near':os_base_near,
-                 };
-                  rpc.query({
-                    model: 'dr.prescription',
-                    method: 'create_product_pos',
-                    args: [product_vals],
-                }).then(function (products){
-                        self.pos.db.add_optical_orders(products)
-                        $('.optical_prescription').text(products.name);
-                        order.set_optical_reference(products);
-                        console.log(products)
-                });
-                },
+                    confirm: function(){
+                        rpc.query({
+                            model: 'dr.prescription',
+                            method: 'create_product_pos',
+                            args: [vals],
+                        }).then(function (products){
+                            self.pos.db.add_optical_orders(products)
+                            $('.optical_prescription').text(products.name);
+                            order.set_optical_reference(products);
+                            console.log(products)
+                        });
+                    },
             });
-    };
-     },
-
+        };
+    },
     click_cancel: function(){
         this.gui.close_popup();
         if (this.options.cancel) {
             this.options.cancel.call(this);
         }
     },
-
 });
 gui.define_popup({name:'product_create', widget: ProductCreationWidget});
 
